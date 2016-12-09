4cf036f6823cfc313caf7d246016ec7bfe2ae528
Christopher McCullough <mcculloughjchris@gmail.com>
2016-12-09 10:35:14 -0500

    a lot of things

--- commands.js
+++ commands.js
@@ -3,10 +3,51 @@ function send(client, command) {
     client.write(command + '\n');
 }
 
+function checkDB(client) {
+    'use strict';
+    if (client !== undefined) {
+        if (client.bot !== undefined) {
+            if (client.bot.db !== undefined) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 function echo(client, msg) {
     'use strict';
-    var command = 'PRIVMSG ' + msg.channel + ' :' + String(msg.msg).replace('echo ', '');
-    send(client, command);
+    if (msg.msg !== undefined && msg.msg.length > 0 && msg.channel !== undefined && msg.channel.length > 0) {
+        var command = 'PRIVMSG ' + msg.channel + ' :' + String(msg.msg).replace('echo ', '');
+        send(client, command);
+    }
+}
+
+function points(client, msg) {
+    'use strict';
+    var cache = {};
+    msg.msg = String(msg.msg).trim();
+    if (msg.msg === 'points') {
+        // do nothing rn
+    } else {
+        cache.user = String(msg.msg).replace('points ', '');
+        if (String(cache.user).length > 0) {
+            if (checkDB(client)) {
+                if (client.bot.db.points !== undefined) {
+                    if (client.bot.db.points.hasOwnProperty(cache.user)) {
+                        if (msg.channel !== undefined && msg.channel.length > 0) {
+                            cache.command = 'PRIVMSG ' + msg.channel + ' :' + cache.user + ' has ' + client.bot.db.points[cache.user] + ' point';
+                            if (client.bot.db.points[cache.user] !== 1) {
+                                cache.command += 's';
+                            }
+                            send(client, cache.command);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 module.exports.echo = echo;
+module.exports.points = points;
--- index.js
+++ index.js
@@ -1,81 +1,110 @@
+/**
+ * get requirements and setup client
+ */
+var json = require('jsonfile');
 var net = require('net');
 var client = net.Socket();
 
+/**
+ * make some bot config
+ */
 var bot = {
+    path: String(__dirname + '/'),
     name: 'dogebot2',
     autojoin: [
         '#DogeTester'
-    ]
+    ],
+    json: json
 };
 
-var cache = {};
-
-client.connect({
-    port: 6667,
-    host: 'chat.freenode.net'
-}, function (data) {
+/**
+ * read db
+ */
+function readDB(callback) {
     'use strict';
-    console.log('connected');
-});
-client.setNoDelay(true);
-
-var commands = require('./commands'),
-    i;
-
-function send(command) {
-    'use strict';
-    return client.write(command + '\n');
+    json.readFile(bot.path + 'db.json', function (err, obj) {
+        if (obj) {
+            bot.db = obj;
+        } else {
+            bot.db = {};
+        }
+        callback();
+    });
 }
 
-function handleData(data) {
+readDB(function () {
     'use strict';
-    var i;
-
-    console.log('Received: ' + data);
+    var cache = {};
 
     /**
-     * Handle long and autojoin
+     * connect to freenode
      */
-    if (data.indexOf('Found your hostname') !== -1) {
-        send('NICK ' + bot.name);
-        send('USER ' + bot.name + ' 8 * : ' + bot.name);
+    client.connect({
+        port: 6667,
+        host: 'chat.freenode.net'
+    }, function (data) {
+        console.log('connected');
+    });
+    client.setNoDelay(true);
 
-        for (i = 0; i < bot.autojoin.length; i += 1) {
-            send('JOIN ' + bot.autojoin[i]);
-            send('PRIVMSG ' + bot.autojoin[i] + ' :sup cunts');
+    client.bot = bot;
+
+
+    var commands = require('./commands'),
+        i;
+
+    function send(command) {
+        return client.write(command + '\n');
+    }
+
+    function handleData(data) {
+        console.log('Received: ' + data);
+
+        /**
+         * Handle long and autojoin
+         */
+        if (data.indexOf('Found your hostname') !== -1) {
+            send('NICK ' + bot.name);
+            send('USER ' + bot.name + ' 8 * : ' + bot.name);
+
+            for (i = 0; i < bot.autojoin.length; i += 1) {
+                send('JOIN ' + bot.autojoin[i]);
+                send('PRIVMSG ' + bot.autojoin[i] + ' :sup cunts');
+            }
         }
-    }
 
-    /**
-     * Prevent timeout
-     */
-    if (data.indexOf('PING :') !== -1) {
-        cache.pingBackTo = String(data).replace('PING ', '');
-        send('PONG ' + cache.pingBackTo);
-    }
+        /**
+         * Prevent timeout
+         */
+        if (data.indexOf('PING :') !== -1) {
+            cache.pingBackTo = String(data).replace('PING ', '');
+            send('PONG ' + cache.pingBackTo);
+        }
 
-    /**
-     * Handles MSGS
-     * I'll need this: (\:.*\!).*(PRIVMSG).*(\#.* :)(.*)
-     */
-    if (data.indexOf('PRIVMSG') !== -1) {
-        cache.privmsgRegex = /(\:.*\!).*(PRIVMSG).*(\#.* :)(.*)/i;
-        cache.found = String(data).match(cache.privmsgRegex);
-        cache.currentMsg = {};
-        if (cache.found !== null) {
-            cache.currentMsg.user = String(cache.found[1]).replace(':', '').replace('!', '');
-            cache.currentMsg.channel = String(cache.found[3]).replace(' :', '');
-            cache.currentMsg.msg = String(cache.found[4]);
+        /**
+         * Handles MSGS
+         * I'll need this: (\:.*\!).*(PRIVMSG).*(\#.* :)(.*)
+         */
+        if (data.indexOf('PRIVMSG') !== -1) {
+            cache.privmsgRegex = /(\:.*\!).*(PRIVMSG).*(\#.* :)(.*)/i;
+            cache.found = String(data).match(cache.privmsgRegex);
+            cache.currentMsg = {};
+            if (cache.found !== null) {
+                cache.currentMsg.user = String(cache.found[1]).replace(':', '').replace('!', '');
+                cache.currentMsg.channel = String(cache.found[3]).replace(' :', '');
+                cache.currentMsg.msg = String(cache.found[4]);
 
-            for (i in commands) {
-                if (commands.hasOwnProperty(i)) {
-                    if (cache.currentMsg.msg.indexOf(i) === 0) {
-                        commands[i](client, cache.currentMsg);
+                for (i in commands) {
+                    if (commands.hasOwnProperty(i)) {
+                        if (cache.currentMsg.msg.indexOf(i) === 0) {
+                            commands[i](client, cache.currentMsg);
+                        }
                     }
                 }
             }
         }
     }
-}
 
-client.on('data', handleData);
+    client.on('data', handleData);
+});
+
