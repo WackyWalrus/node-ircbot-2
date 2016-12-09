0310c7cdc3fe9d472aa9c751238322df46df4a92
Christopher McCullough <mcculloughjchris@gmail.com>
2016-12-09 15:22:29 -0500

    only admins can reload files

--- index.js
+++ index.js
@@ -124,7 +124,9 @@ readDB(function () {
                     /**
                      * if this is just asking the bot to reload
                      */
-                    if (cache.currentMsg.msg === bot.name + ' reload the thing' && !client.bot.db.ignored.hasOwnProperty(cache.currentMsg.user)) {
+                    if (cache.currentMsg.msg === bot.name + ' reload the thing' &&
+                            !client.bot.db.ignored.hasOwnProperty(cache.currentMsg.user) &&
+                            client.bot.db.admins.hasOwnProperty(cache.currentMsg.user)) {
                         client.bot.required = {};
                         /**
                          * go through the 'require' module, find the required files a delete them from the cache
